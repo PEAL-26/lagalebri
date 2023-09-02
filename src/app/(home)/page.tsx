28b2466c3372e92ef77731a01108077173c2d4c4
peal-26 <edilasio@live.com>
2023-09-02 16:27:20 +0100

    update metadata home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,11 @@
 import { Banner } from "@/components/home/banner";
 
+export const metadata = {
+  title: "Página Principal",
+  description:
+    "Seu lar perfeito e soluções tecnológicas de ponta. Concretize seus projetos e tenha a casa dos seus sonhos com a Lagalebri - sua parceira confiável em construção e tecnologia.",
+};
+
 export default function HomePage() {
   return (
     <>
